7b7ef1d12ac9f83748e9553a045c52e12aa9f780
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-05 17:19:09 +0000

    Create menu selection.py

---
+++ menu selection.py
@@ -0,0 +1,11 @@
+a = 0
+while(a == 0):
+  print("1 = play game")
+  print("2 = instructions")
+  print("3 = quit ")
+  b = input("")
+  if b == "1" or b == "2" or b == "3":
+    print("lets go")
+    a = 1
+  else:
+    print("\n")
