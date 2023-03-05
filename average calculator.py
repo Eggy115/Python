a99f6321b87f18ec2740d5a3e04b08f33ad3cdd1
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-05 17:16:52 +0000

    Create average calculator.py

---
+++ average calculator.py
@@ -0,0 +1,10 @@
+number = input("how many numbers ")
+number = int(number)
+B = 0
+B = int(B)
+for i in range(number):
+  A = input("whats the number? ")
+  A = int(A)
+  B = B + A
+answer = B / number
+print(answer)
