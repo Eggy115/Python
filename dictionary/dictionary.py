b9d2054888b632a4968a25b035ad259aad116609
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-05 16:38:22 +0000

    Create dictionary.py

---
+++ dictionary/dictionary.py
@@ -0,0 +1,11 @@
+a = input("what do u want to find: ")
+text_file = open("words.txt", "r")
+lines = open('words.txt').read().splitlines() 
+counter = 0
+found = False
+while found == False and counter !=370103:
+  if a == lines[counter]:
+    print(lines[counter] + " is word")
+    print("also:", lines[counter + 1], lines[counter - 1], lines[counter + 2], lines[counter - 2])
+    found = True
+  counter = counter + 1
