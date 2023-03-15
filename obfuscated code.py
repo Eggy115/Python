426a42fa3e8e9eeee1a0394160d49cfc0e676b3e
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-15 16:38:05 +0000

    Update obfuscated code.py

--- obfuscated code.py
+++ obfuscated code.py
@@ -1,8 +1,9 @@
 import base64
+# encode code
 a = str(base64.b64encode(b"print('hi')"))[2:]
 a = a[:-1]
-
-a = "hidden code here"
-
+#decode code here
+a = "hidden code here" # add your own encoded code here
 var = base64.b64decode(a)
 exec(var)
+# can be used with base85, 32, 16 etc.
